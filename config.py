f58559638dedf13578559eb8a96031877c6ebee0
urashin <s.urakawa@hotmail.co.jp>
2025-06-08 21:35:50 +0900

    fix:prompt

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ LINE_CHANNEL_ACCESS_TOKEN = "basicコースでは使いません"
 
 # OpenAI の Chat Completions API で使用するモデル名
 # `.env` の OPENAI_MODEL があればそれを、無ければ `gpt-4o` を採用
-AI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o")
+AI_MODEL = "gpt-4o"
 
 AI_URL = "http://host.docker.internal:11434"
 
@@ -29,3 +29,5 @@ DB_PORT = 3306
 # VOICEBOXエンジンのURL
 VOICEBOX_URL = "http://voicebox:50021"
 
+VOICEBOX_SPEAKER=14
+VOICEBOX_SPEED=2.0
